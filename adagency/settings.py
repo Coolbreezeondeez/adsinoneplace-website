66c4106c171d7e3021fb285ebea1f9fb971e6576
Coolbreezeondeez <srbenbrucker@gmail.com>
2025-11-23 08:33:51 -0800

    Fixed PostgreSQL configuration - removed duplicate DATABASES

--- adagency/settings.py
+++ adagency/settings.py
@@ -92,12 +92,6 @@ DATABASES = {
         conn_max_age=600
     )
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
