59f31c4e890e0b7195f71ea075f009d7154c1f8f
Coolbreezeondeez <139991330+Coolbreezeondeez@users.noreply.github.com>
2026-01-07 12:31:28 -0800

    Clean up sitemaps.py by removing comments
    Removed commented code and simplified lastmod method.

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -8,10 +8,7 @@ class BlogSitemap(Sitemap):
     priority = 0.8
 
     def items(self):
-        # This returns all posts. 
-        # If you have a 'status' field, use: Post.objects.filter(status='published')
         return Post.objects.all().order_by('-created_at')
 
-    def lastmod(self, obj):
-        # This tells Google when the post was last updated
-        return obj.updated_at
+   def lastmod(self, obj):
+    return obj.updated_at
