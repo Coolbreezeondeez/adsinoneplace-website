ee5a42358c188ddc110f8c623ccace1e46104643
Coolbreezeondeez <139991330+Coolbreezeondeez@users.noreply.github.com>
2026-01-07 08:33:49 -0800

    Add django.contrib.sites and django.contrib.sitemaps

--- adagency/settings.py
+++ adagency/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',    
+    'django.contrib.sitemaps', 
+]
+
+SITE_ID = 1
     
     # Custom apps
     'core',
