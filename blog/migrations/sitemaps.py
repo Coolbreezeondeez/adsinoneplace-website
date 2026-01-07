13d6e97131223501cd5b290336572009ad1fde8a
Coolbreezeondeez <139991330+Coolbreezeondeez@users.noreply.github.com>
2026-01-07 09:04:17 -0800

    Implement BlogSitemap for blog posts
    Added BlogSitemap class for generating sitemaps.

--- blog/migrations/sitemaps.py
+++ blog/migrations/sitemaps.py
@@ -1,1 +1,17 @@
+# blog/sitemaps.py
 
+from django.contrib.sitemaps import Sitemap
+from .models import Post  # <--- CHECK THIS: Is your model named 'Post'?
+
+class BlogSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # This returns all posts. 
+        # If you have a 'status' field, use: Post.objects.filter(status='published')
+        return Post.objects.all().order_by('-created_on')
+
+    def lastmod(self, obj):
+        # This tells Google when the post was last updated
+        return obj.updated_on
